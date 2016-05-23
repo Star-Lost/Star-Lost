251d97d2cad55f2c59aa3859c27508b8db99dc84
Mathias Pius Damm-Pedersen <MathiasPius@hotmail.com>
2016-05-23 15:10:09 +0200

    Added animations and simple controls

--- Star-Lost/main.cpp
+++ Star-Lost/main.cpp
@@ -24,48 +24,115 @@ int main()
 
 	model char_model{
 		// Standing still facing south animation
-		std::make_pair(std::string{ "still_south" }, std::initializer_list<model::animation::frame>{
+		std::make_pair(std::string{ "stand_south" }, std::initializer_list<model::animation::frame>{
 			{ char_tex, sf::IntRect(0, 0, 16, 16)}
 		}),
+
+		// Standing still facing north animation
+		std::make_pair(std::string{ "stand_north" }, std::initializer_list<model::animation::frame>{
+			{ char_tex, sf::IntRect(16, 0, 16, 16)}
+		}),
+
+		// Standing still facing west animation
+		std::make_pair(std::string{ "stand_west" }, std::initializer_list<model::animation::frame>{
+			{ char_tex, sf::IntRect(32, 0, 16, 16)}
+		}),
+
+		// Standing still facing east animation
+		std::make_pair(std::string{ "stand_east" }, std::initializer_list<model::animation::frame>{
+			{ char_tex, sf::IntRect(48, 0, 16, 16)}
+		}),
+
 		// Walk south animation
 		std::make_pair(std::string{ "walk_south" }, std::initializer_list<model::animation::frame>{
 			{ char_tex, sf::IntRect(0, 16, 16, 16) },
 			{ char_tex, sf::IntRect(0, 32, 16, 16) },
-			{ char_tex, sf::IntRect(0, 0, 16, 16) },
+			{ char_tex, sf::IntRect(0,  0, 16, 16) },
 			{ char_tex, sf::IntRect(0, 48, 16, 16) },
 			{ char_tex, sf::IntRect(0, 64, 16, 16) },
-			{ char_tex, sf::IntRect(0, 0, 16, 16)}
+			{ char_tex, sf::IntRect(0,  0, 16, 16)}
+		}),
+
+		// Walk north animation
+		std::make_pair(std::string{ "walk_north" }, std::initializer_list<model::animation::frame>{
+			{ char_tex, sf::IntRect(16, 16, 16, 16) },
+			{ char_tex, sf::IntRect(16, 32, 16, 16) },
+			{ char_tex, sf::IntRect(16,  0, 16, 16) },
+			{ char_tex, sf::IntRect(16, 48, 16, 16) },
+			{ char_tex, sf::IntRect(16, 64, 16, 16) },
+			{ char_tex, sf::IntRect(16,  0, 16, 16) }
+		}),
+
+		// Walk west animation
+		std::make_pair(std::string{ "walk_west" }, std::initializer_list<model::animation::frame>{
+			{ char_tex, sf::IntRect(32, 16, 16, 16) },
+			{ char_tex, sf::IntRect(32, 32, 16, 16) },
+			{ char_tex, sf::IntRect(32,  0, 16, 16) },
+			{ char_tex, sf::IntRect(32, 48, 16, 16) },
+			{ char_tex, sf::IntRect(32, 64, 16, 16) },
+			{ char_tex, sf::IntRect(32,  0, 16, 16) }
+		}),
+
+		// Walk east animation
+		std::make_pair(std::string{ "walk_east" }, std::initializer_list<model::animation::frame>{
+			{ char_tex, sf::IntRect(48, 16, 16, 16) },
+			{ char_tex, sf::IntRect(48, 32, 16, 16) },
+			{ char_tex, sf::IntRect(48,  0, 16, 16) },
+			{ char_tex, sf::IntRect(48, 48, 16, 16) },
+			{ char_tex, sf::IntRect(48, 64, 16, 16) },
+			{ char_tex, sf::IntRect(48,  0, 16, 16) }
 		}),
 	};
 	// Character animation stuff ends here
 
 	sf::Clock clock;
 	// run the program as long as the window is open
+
+	// Draw the character
+	const model::animation *anim = char_model["stand_south"];
+
 	while (window.isOpen())
 	{
 		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				window.close();
+			switch (event.type)
+			{
+			case sf::Event::Closed:	window.close(); break;
+
+			case sf::Event::KeyPressed:
+			{
+				switch(event.key.code)
+				{
+					case sf::Keyboard::W: anim = char_model["walk_north"]; break;
+					case sf::Keyboard::A: anim = char_model["walk_west"]; break;
+					case sf::Keyboard::S: anim = char_model["walk_south"]; break;
+					case sf::Keyboard::D: anim = char_model["walk_east"]; break;
+					default: break;
+				}
+				break;
+			}
+
+			case sf::Event::KeyReleased:
+			{
+				switch (event.key.code)
+				{
+				case sf::Keyboard::W: anim = char_model["stand_north"]; break;
+				case sf::Keyboard::A: anim = char_model["stand_west"]; break;
+				case sf::Keyboard::S: anim = char_model["stand_south"]; break;
+				case sf::Keyboard::D: anim = char_model["stand_east"]; break;
+				default: break;
+				}
+				break;
+			}
+
+			default: break;
+			}
 		}
 
 		// clear the window with black color
 		window.clear(sf::Color::Black);
-
-
-
-
-		// Draw the character
-		const model::animation *anim = nullptr;
-
-		// We switch between standing still and walking south every 5 seconds
-		if (static_cast<int>(clock.getElapsedTime().asSeconds()) % 10 > 5)
-			anim = char_model["still_south"];
-		else
-			anim = char_model["walk_south"];
 
 		// Then get the current frame of the animation
 		const model::animation::frame &curframe =
@@ -73,7 +140,7 @@ int main()
 		
 		character.setTexture(*curframe.get_texture());
 		character.setTextureRect(curframe.get_subtexture());
-		character.setPosition(sf::Vector2f(700.0f, 200.0f + (clock.getElapsedTime().asMilliseconds() % 5000) / 50.0f));
+		character.setPosition(sf::Vector2f(20.0f, 20.0f)); // +(clock.getElapsedTime().asMilliseconds() % 5000) / 50.0f));
 		window.draw(character);
 
 		// end the current frame
