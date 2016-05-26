8a4babeff0104dd65ab567ecd8e9cdeb317569db
Mathias Pius Damm-Pedersen <MathiasPius@hotmail.com>
2016-05-26 14:06:56 +0200

    Fixed resource paths for proper directory
    Change your working directories to ..\Build\Debug and ..\Build\Release
    respectively.

--- Star-Lost/resources.cpp
+++ Star-Lost/resources.cpp
@@ -2,7 +2,7 @@
 #include <SFML/Audio.hpp>
 #include "resources.h"
 
-const std::string thirdparty_resource_path = "..\\..\\Resources\\";
-const std::string internal_resource_path = "..\\..\\..\\Resources\\";
+const std::string thirdparty_resource_path = "..\\..\\Third party\\Resources\\";
+const std::string internal_resource_path = "..\\..\\Resources\\";
 
 const std::string resource_loader<sf::SoundBuffer>::path = "Sounds\\";
