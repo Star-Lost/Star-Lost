c5c25b4067c34dba4e18ad911cf8534d8cff22f0
Mathias Pius Damm-Pedersen <MathiasPius@hotmail.com>
2017-07-09 21:02:31 +0200

    Updated relative resource paths
    For some reason the default working directory changed in VS2017. Should probably look into locking it down.

--- Star-Lost/resources.cpp
+++ Star-Lost/resources.cpp
@@ -2,8 +2,8 @@
 #include <SFML/Audio.hpp>
 #include "resources.h"
 
-const std::string thirdparty_resource_path = "..\\..\\Third party\\Resources\\";
-const std::string internal_resource_path = "..\\..\\Resources\\";
+const std::string thirdparty_resource_path = "..\\Third party\\Resources\\";
+const std::string internal_resource_path = "..\\Resources\\";
 
 const std::string resource_loader<sf::SoundBuffer>::path = "Sounds\\";
 const std::string resource_loader<sf::Font>::path = "Fonts\\";
